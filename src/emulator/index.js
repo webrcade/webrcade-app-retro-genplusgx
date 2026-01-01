3cd3a7cbaa411fe140694004e611d725bc541964
raz0red <raz0red@gmail.com>
2026-01-01 10:09:58 -0800

    update audio

--- src/emulator/index.js
+++ src/emulator/index.js
@@ -18,7 +18,19 @@ export class Emulator extends RetroAppWrapper {
     this.frequency = 60;
 
     this.audioStarted = 0;
+    this.total = 0;
+    this.count = 0;
+
     this.audioCallback = (offset, length) => {
+      this.total += length;
+      this.count = this.count + 1;
+
+      if (this.count === 60) {
+        //console.log("total: " + this.total);
+        this.total = 0;
+        this.count = 0;
+      }
+
       length = length << 1;
       const audioArray = new Int16Array(window.Module.HEAP16.buffer, offset, length);
       this.audioProcessor.storeSoundCombinedInput(audioArray, 2, length, 0, 32768);
@@ -50,8 +62,8 @@ export class Emulator extends RetroAppWrapper {
     return new ScriptAudioProcessor(
       2,
       48000,
-      32768,
-      4096
+      8192 + 4096,
+      2048
     ).setDebug(this.debug);
   }
 
